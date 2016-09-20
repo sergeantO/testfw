893610d38ba0a105e72b4f6a5cc677bd409da88c
sergeantO <morgueheadman@gmail.com>
2016-09-20 16:03:21 +0300

    mvc 2

---
+++ controllers/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+require_once ROOT . '/models/News.php';
+
+class NewsController {
+    public function actionGetAll(){
+        $items = News::getAll();
+        include ROOT . '/views/news/all.php';
+    }
+    
+    public function actionGetOne(){
+        $id = $_GET['id'];
+        $item = News::getOne($id);
+        include ROOT . '/views/news/one.php';
+    }
+}
